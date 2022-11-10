ea2f28228225cf20a59813f0fc904090e756beb2
Yohsuke Murase <yohsuke.murase@gmail.com>
2022-11-10 09:55:33 +0100

    [WIP] leading eight

--- Norm.hpp
+++ Norm.hpp
@@ -2,6 +2,7 @@
 #define NORM_HPP
 
 #include <iostream>
+#include <iomanip>
 #include <string>
 #include <sstream>
 #include <vector>
@@ -56,7 +57,10 @@ class ActionRule {
   ActionRule(const std::array<double,4>& coop_probs) : coop_probs(coop_probs) {};
   // {P(C|B,B), P(C|B,G), P(C|G,B), P(C|G,G)}
 
+  std::array<double,4> coop_probs; // P(C|B,B), P(C|B,G), P(C|G,B), P(C|G,G)
+
   ActionRule Clone() const { return ActionRule(coop_probs); }
+
   double CProb(const Reputation& rep_d, const Reputation& rep_r) const {
     size_t idx = static_cast<size_t>(rep_d) * 2 + static_cast<size_t>(rep_r);
     return coop_probs[idx];
@@ -72,7 +76,7 @@ class ActionRule {
 
   std::string Inspect() const {
     std::stringstream ss;
-    ss << "ActionRule:" << std::endl;
+    ss << "ActionRule: " << ID() << std::endl;
     for (size_t i = 0; i < 4; i++) {
       Reputation rep_d = static_cast<Reputation>(i / 2);
       Reputation rep_r = static_cast<Reputation>(i % 2);
@@ -88,7 +92,24 @@ class ActionRule {
     else { return false; }
   }
 
-  std::array<double,4> coop_probs; // P(C|B,B), P(C|B,G), P(C|G,B), P(C|G,G)
+  bool IsDeterministic() const {
+    if (( coop_probs[0] == 0.0 || coop_probs[0] == 1.0 ) &&
+        ( coop_probs[1] == 0.0 || coop_probs[1] == 1.0 ) &&
+        ( coop_probs[2] == 0.0 || coop_probs[2] == 1.0 ) &&
+        ( coop_probs[3] == 0.0 || coop_probs[3] == 1.0 ) ) {
+      return true;
+    }
+    return false;
+  }
+
+  int ID() const {
+    if (!IsDeterministic()) { return -1; }
+    int id = 0;
+    for (size_t i = 0; i < 4; i++) {
+      if (coop_probs[i] == 1.0) { id += 1 << i; }
+    }
+    return id;
+  }
 
   static ActionRule MakeDeterministicRule(int id) {
     if (id < 0 || id > 15) {
@@ -101,18 +122,36 @@ class ActionRule {
     }
     return ActionRule{ c_probs };
   }
+
+  static ActionRule DISC() {
+    return ActionRule{ {0.0, 1.0, 0.0, 1.0} };
+  }
+  static ActionRule ALLC() {
+    return ActionRule{ {1.0, 1.0, 1.0, 1.0} };
+  }
+  static ActionRule ALLD() {
+    return ActionRule{ {0.0, 0.0, 0.0, 0.0} };
+  }
 };
 
+bool operator==(const ActionRule& t1, const ActionRule& t2) {
+  return t1.coop_probs[0] == t2.coop_probs[0] &&
+         t1.coop_probs[1] == t2.coop_probs[1] &&
+         t1.coop_probs[2] == t2.coop_probs[2] &&
+         t1.coop_probs[3] == t2.coop_probs[3];
+}
+bool operator!=(const ActionRule& t1, const ActionRule& t2) { return !(t1 == t2); }
 
 class AssessmentRule {
   public:
   AssessmentRule(const std::array<double,8>& g_probs) : good_probs(g_probs) {};
   // {P(G|B,B,D), P(G|B,B,C), P(G|B,G,D), P(G|B,G,C), P(G|G,B,D), P(G|G,B,C), P(G|G,G,D), P(G|G,G,C)}
+  std::array<double,8> good_probs;
 
   AssessmentRule Clone() const { return AssessmentRule(good_probs); }
   AssessmentRule SwapGB() const {
-    std::array<double,8> new_good_probs = {good_probs[6], good_probs[7], good_probs[4], good_probs[5],
-                                           good_probs[2], good_probs[3], good_probs[0], good_probs[1]};
+    std::array<double,8> new_good_probs = {1.0-good_probs[6], 1.0-good_probs[7], 1.0-good_probs[4], 1.0-good_probs[5],
+                                           1.0-good_probs[2], 1.0-good_probs[3], 1.0-good_probs[0], 1.0-good_probs[1]};
     return {new_good_probs};
   }
   double GProb(const Reputation& rep_d, const Reputation& rep_r, const Action& act) const {
@@ -126,7 +165,7 @@ class AssessmentRule {
 
   std::string Inspect() const {
     std::stringstream ss;
-    ss << "AssessmentRule:" << std::endl;
+    ss << "AssessmentRule: " << ID() << std::endl;
     for (size_t i = 0; i < 8; i++) {
       Reputation rep_d = static_cast<Reputation>(i / 4);
       Reputation rep_r = static_cast<Reputation>((i/2) % 2);
@@ -144,8 +183,28 @@ class AssessmentRule {
     else { return false; }
   }
 
-  std::array<double,8> good_probs;
-  // {P(G|B,B,D), P(G|B,B,C), P(G|B,G,D), P(G|B,G,C), P(G|G,B,D), P(G|G,B,C), P(G|G,G,D), P(G|G,G,C)}
+  bool IsDeterministic() const {
+    if (( good_probs[0] == 0.0 || good_probs[0] == 1.0 ) &&
+        ( good_probs[1] == 0.0 || good_probs[1] == 1.0 ) &&
+        ( good_probs[2] == 0.0 || good_probs[2] == 1.0 ) &&
+        ( good_probs[3] == 0.0 || good_probs[3] == 1.0 ) &&
+        ( good_probs[4] == 0.0 || good_probs[4] == 1.0 ) &&
+        ( good_probs[5] == 0.0 || good_probs[5] == 1.0 ) &&
+        ( good_probs[6] == 0.0 || good_probs[6] == 1.0 ) &&
+        ( good_probs[7] == 0.0 || good_probs[7] == 1.0 ) ) {
+      return true;
+    }
+    return false;
+  }
+
+  int ID() const {
+    if (!IsDeterministic()) { return -1; }
+    int id = 0;
+    for (size_t i = 0; i < 8; i++) {
+      if (good_probs[i] == 1.0) { id += 1 << i; }
+    }
+    return id;
+  }
 
   static AssessmentRule MakeDeterministicRule(int id) {
     if (id < 0 || id > 255) {
@@ -158,41 +217,166 @@ class AssessmentRule {
     }
     return AssessmentRule{ good_probs };
   }
+
+  static AssessmentRule AllGood() {
+    return AssessmentRule::MakeDeterministicRule(0b11111111);
+  }
+  static AssessmentRule AllBad() {
+    return AssessmentRule::MakeDeterministicRule(0b00000000);
+  }
+  static AssessmentRule ImageScoring() {
+    return AssessmentRule::MakeDeterministicRule(0b10101010);
+  }
 };
 
-
+bool operator==(const AssessmentRule& t1, const AssessmentRule& t2) {
+  return t1.good_probs[0] == t2.good_probs[0] &&
+         t1.good_probs[1] == t2.good_probs[1] &&
+         t1.good_probs[2] == t2.good_probs[2] &&
+         t1.good_probs[3] == t2.good_probs[3] &&
+         t1.good_probs[4] == t2.good_probs[4] &&
+         t1.good_probs[5] == t2.good_probs[5] &&
+         t1.good_probs[6] == t2.good_probs[6] &&
+         t1.good_probs[7] == t2.good_probs[7];
+}
+bool operator!=(const AssessmentRule& t1, const AssessmentRule& t2) {
+  return !(t1 == t2);
+}
 
 // Norm is a set of AssessmentRule & ActionRule
 class Norm {
   public:
-  Norm(const AssessmentRule& rep_d, const ActionRule& act_r) : rd(rep_d.good_probs), ar(act_r.coop_probs) {};
-  Norm(const Norm& rhs) : rd(rhs.rd), ar(rhs.ar) {};
-  AssessmentRule rd;
-  ActionRule ar;
+  Norm(const AssessmentRule& R_d, const AssessmentRule& R_r, const ActionRule& act)
+  : Rd(R_d.good_probs), Rr(R_r.good_probs), P(act.coop_probs) {};
+  Norm(const Norm& rhs) : Rd(rhs.Rd), Rr(rhs.Rr), P(rhs.P) {};
+  AssessmentRule Rd, Rr;  // assessment rules to assess donor and recipient
+  ActionRule P;  // action rule
   std::string Inspect() const {
     std::stringstream ss;
-    for (int i = 0; i < 4; i++) {
+    if (IsDeterministic()) {
+      ss << "Norm: " << ID() << std::endl;
+      for (int i = 3; i >= 0; i--) {
+        Reputation X = static_cast<Reputation>(i / 2);
+        Reputation Y = static_cast<Reputation>(i % 2);
+        Action A = (CProb(X,Y) == 1.0) ? Action::C : Action::D;
+        Action notA = FlipAction(A);
+        Reputation donor_rep = Rd.GProb(X, Y, A) == 1.0 ? Reputation::G : Reputation::B;
+        Reputation donor_rep_not = Rd.GProb(X, Y, notA) == 1.0 ? Reputation::G : Reputation::B;
+        Reputation recip_rep = Rr.GProb(X, Y, A) == 1.0 ? Reputation::G : Reputation::B;
+        Reputation recip_rep_not = Rr.GProb(X, Y, notA) == 1.0 ? Reputation::G : Reputation::B;
+        ss << "(" << X << "->" << Y << "):" << A << donor_rep << donor_rep_not << ":" << recip_rep << recip_rep_not << "\t";
+      }
+      ss << "\n";
+    }
+    for (int i = 3; i >= 0; i--) {
       Reputation X = static_cast<Reputation>(i/2);
       Reputation Y = static_cast<Reputation>(i%2);
-      auto p = At(X, Y);
-      ss << "(" << X << "->" << Y << "): " << std::get<0>(p) << std::get<1>(p) << ':' << std::get<2>(p);
-      ss << ((i % 3 == 2) ? "\n" : "\t");
+      double c_prob = P.CProb(X, Y);
+      double gd_prob_d = Rd.GProb(X, Y, Action::D);
+      double gd_prob_c = Rd.GProb(X, Y, Action::C);
+      double gr_prob_d = Rr.GProb(X, Y, Action::D);
+      double gr_prob_c = Rr.GProb(X, Y, Action::C);
+      ss << std::setprecision(2) << std::fixed;
+      ss << "(" << X << "->" << Y << "): "
+          << "c_prob:" << c_prob << " : "
+          << "donor_gprob (c:" << gd_prob_c << ",d:" << gd_prob_d << ") : "
+          << "recip_gprob (c:" << gr_prob_c << ",d:" << gr_prob_d << ")\n";
     }
     return ss.str();
   }
-  std::tuple<double,double,double> At(Reputation donor, Reputation recipient) const {
-    double c_prob = ar.CProb(donor, recipient);
-    double g_prob_c = rd.GProb(donor, recipient, Action::C);
-    double g_prob_d = rd.GProb(donor, recipient, Action::D);
-    return std::make_tuple(c_prob, g_prob_c, g_prob_d);
-  }
-  double CProb(Reputation donor, Reputation recipient) const { return ar.CProb(donor, recipient); }
-  double GProb(Reputation donor, Reputation recipient, Action act) const { return rd.GProb(donor, recipient, act); }
+  double CProb(Reputation donor, Reputation recipient) const { return P.CProb(donor, recipient); }
+  double GProbDonor(Reputation donor, Reputation recipient, Action act) const { return Rd.GProb(donor, recipient, act); }
+  double GProbRecip(Reputation donor, Reputation recipient, Action act) const { return Rr.GProb(donor, recipient, act); }
   bool IsSecondOrder() const {
-    return rd.IsSecondOrder() && ar.IsSecondOrder();
+    return P.IsSecondOrder() && Rd.IsSecondOrder() && Rr.IsSecondOrder();
   }
-  static Norm AllC() { return Norm(AssessmentRule{{1,1,1,1,1,1,1,1}},ActionRule{{1,1,1,1}}); }
-  static Norm AllD() { return Norm(AssessmentRule{{0,0,0,0,0,0,0,0}},ActionRule{{0,0,0,0}}); }
+  bool IsDeterministic() const {
+    return P.IsDeterministic() && Rd.IsDeterministic() && Rr.IsDeterministic();
+  }
+  bool IsRecipKeep() const { // recipient's reputation is kept
+    return Rr.IsDeterministic() && Rr.ID() == 0b11001100;
+  }
+  Norm RescaleWithError(double mu_e, double mu_a_donor, double mu_a_recip) const { // return the norm that takes into account error probabilities
+    std::array<double,4> coop_probs = {0.0};
+    for (size_t i = 0; i < 4; i++) {
+      coop_probs[i] = (1.0 - mu_e) * P.coop_probs[i];
+    }
+    std::array<double,8> good_probs_donor = {0.0};
+    std::array<double,8> good_probs_recip = {0.0};
+    for (size_t i = 0; i < 8; i++) {
+      good_probs_donor[i] = (1.0 - 2.0 * mu_a_donor) * Rd.good_probs[i] + mu_a_donor;
+      good_probs_recip[i] = (1.0 - 2.0 * mu_a_recip) * Rr.good_probs[i] + mu_a_recip;
+    }
+    return Norm{ {good_probs_donor}, {good_probs_recip}, {coop_probs} };
+  }
+  int ID() const {
+    if (!IsDeterministic()) { return -1; }
+    int id = 0;
+    id += P.ID() << 16;
+    id += Rd.ID() << 8;
+    id += Rr.ID();
+    return id;
+  }
+  static Norm ConstructFromID(int id) {
+    if (id < 0 || id >= (1<<20)) {
+      throw std::runtime_error("Norm: id must be between 0 and 2^20-1");
+    }
+    int P_id = (id >> 16) & 0xF;
+    int Rd_id = (id >> 8) & 0xFF;
+    int Rr_id = id & 0xFF;
+    return Norm(AssessmentRule::MakeDeterministicRule(Rd_id),
+                AssessmentRule::MakeDeterministicRule(Rr_id),
+                ActionRule::MakeDeterministicRule(P_id));
+  }
+  static Norm AllC() { return Norm(AssessmentRule{{1,1,1,1,1,1,1,1}}, AssessmentRule{{1,1,1,1,1,1,1,1}}, ActionRule{{1,1,1,1}}); }
+  static Norm AllD() { return Norm(AssessmentRule{{0,0,0,0,0,0,0,0}}, AssessmentRule{{0,0,0,0,0,0,0,0}}, ActionRule{{0,0,0,0}}); }
+  static Norm L1() {
+    return Norm({{0,1,0,1,1,1,0,1}},
+                {{0,0,1,1,0,0,1,1}},
+                {{1,1,0,1}});
+  }
+  static Norm L2() {
+    return Norm({{0,1,0,1,1,0,0,1}},
+                {{0,0,1,1,0,0,1,1}},
+                {{1,1,0,1}});
+  }
+  static Norm L3() {
+    return Norm({{1,1,0,1,1,1,0,1}},
+                {{0,0,1,1,0,0,1,1}},
+                {{0,1,0,1}});
+  }
+  static Norm L4() {
+    return Norm({{1,0,0,1,1,1,0,1}},
+                {{0,0,1,1,0,0,1,1}},
+                {{0,1,0,1}});
+  }
+  static Norm L5() {
+    return Norm({{1,1,0,1,1,0,0,1}},
+                {{0,0,1,1,0,0,1,1}},
+                {{0,1,0,1}});
+  }
+  static Norm L6() {
+    return Norm({{1,0,0,1,1,0,0,1}},
+                {{0,0,1,1,0,0,1,1}},
+                {{0,1,0,1}});
+  }
+  static Norm L7() {
+    return Norm({{0,0,0,1,1,1,0,1}},
+                {{0,0,1,1,0,0,1,1}},
+                {{0,1,0,1}});
+  }
+  static Norm L8() {
+    return Norm({{0,0,0,1,1,0,0,1}},
+                {{0,0,1,1,0,0,1,1}},
+                {{0,1,0,1}});
+  }
 };
+
+bool operator==(const Norm& n1, const Norm& n2) {
+  return n1.P == n2.P && n1.Rd == n2.Rd && n1.Rr == n2.Rr;
+}
+bool operator!=(const Norm& n1, const Norm& n2) {
+  return !(n1 == n2);
+}
 
 #endif
---
+++ test_Norm.cpp
@@ -0,0 +1,160 @@
+#include <iostream>
+#include <cassert>
+#include "Norm.hpp"
+
+
+template <typename T>
+bool IsAllClose(T a, T b, double epsilon = 0.0001) {
+  for (size_t i = 0; i < a.size(); i++) {
+    if (std::abs(a[i] - b[i]) > epsilon) {
+      return false;
+    }
+  }
+  return true;
+}
+
+void test_ActionRule() {
+  ActionRule P({0.1, 0.2, 0.5, 0.9});
+  std::cout << P.Inspect();
+
+  assert( P.CProb(Reputation::B, Reputation::B) == 0.1 );
+  assert( P.CProb(Reputation::G, Reputation::B) == 0.5 );
+  assert(P.IsSecondOrder() == false);
+
+  P.SetCProb(Reputation::B, Reputation::G, 0.3);
+  assert( P.CProb(Reputation::B, Reputation::G) == 0.3 );
+
+  ActionRule P2 = P.SwapGB();
+  assert( P2.CProb(Reputation::G, Reputation::G) == 0.1 );
+  assert( P2.CProb(Reputation::B, Reputation::G) == 0.5 );
+
+  ActionRule P3({0.0, 1.0, 0.0, 1.0});
+  std::cout << P3.Inspect();
+  assert( P3.IsDeterministic() == true );
+  assert( P3.ID() == 10 );
+  assert( P3.IsSecondOrder() == true);
+
+  auto disc = ActionRule::DISC();
+  assert( disc.IsDeterministic() == true );
+  assert( disc.ID() == 10 );
+  auto allc = ActionRule::ALLC();
+  assert( allc.IsDeterministic() == true );
+  assert( allc.ID() == 15 );
+  auto alld = ActionRule::ALLD();
+  assert( alld.IsDeterministic() == true );
+  assert( alld.ID() == 0 );
+
+  auto P4 = ActionRule::MakeDeterministicRule(7);
+  assert( P4.ID() == 7 );
+
+  std::cout << "test_ActionRule passed!" << std::endl;
+}
+
+void test_AssessmentRule() {
+  AssessmentRule R({0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8});
+  std::cout << R.Inspect();
+
+  assert( R.GProb(Reputation::B, Reputation::B, Action::D) == 0.1 );
+  assert( R.GProb(Reputation::B, Reputation::B, Action::C) == 0.2 );
+  assert( R.GProb(Reputation::G, Reputation::B, Action::C) == 0.6 );
+  assert( R.IsSecondOrder() == false);
+
+  R.SetGProb(Reputation::B, Reputation::G, Action::C, 0.9);
+  assert( R.GProb(Reputation::B, Reputation::G, Action::C) == 0.9 );
+
+  AssessmentRule R2 = R.SwapGB();
+  assert( R2.GProb(Reputation::G, Reputation::G, Action::C) == 1.0 - R.GProb(Reputation::B, Reputation::B, Action::C) );
+  assert( R2.GProb(Reputation::B, Reputation::G, Action::D) == 1.0 - R.GProb(Reputation::G, Reputation::B, Action::D) );
+
+  AssessmentRule Q3({0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0});
+  std::cout << Q3.Inspect();
+  assert( Q3.IsDeterministic() == true );
+  assert( Q3.ID() == 0b10101010 );
+  assert( Q3.IsSecondOrder() == true);
+
+  auto allg = AssessmentRule::AllGood();
+  assert( allg.IsDeterministic() == true );
+  assert( allg.ID() == 0b11111111 );
+  auto allb = AssessmentRule::AllBad();
+  assert( allb.IsDeterministic() == true );
+  assert( allb.ID() == 0b00000000 );
+  auto is = AssessmentRule::ImageScoring();
+  assert( is.IsDeterministic() == true );
+  assert( is.ID() == 0b10101010 );
+
+  std::cout << "test_AssessmentRule passed!" << std::endl;
+}
+
+void test_Norm() {
+  ActionRule p = ActionRule::DISC();
+  AssessmentRule r1 = AssessmentRule::ImageScoring();
+  AssessmentRule r2 = AssessmentRule::AllGood();
+
+  std::cout << Norm(r1, r2, p).Inspect();
+
+  Norm n(
+      {{0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7}},
+      {{0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9}},
+      {{0.0,0.25,0.5,0.75}}
+      );
+  assert( n.IsDeterministic() == false );
+  assert( n.IsSecondOrder() == false );
+  assert( n.IsRecipKeep() == false );
+  assert( n.GProbDonor(Reputation::B, Reputation::G, Action::C) == 0.3 );
+  assert( n.GProbRecip(Reputation::B, Reputation::G, Action::C) == 0.5 );
+  assert( n.CProb(Reputation::B, Reputation::G) == 0.25 );
+  std::cout << n.Inspect();
+
+  std::array<Norm,8> leading_eight = {Norm::L1(), Norm::L2(), Norm::L3(), Norm::L4(),
+                                      Norm::L5(), Norm::L6(), Norm::L7(), Norm::L8()};
+  std::array<int,8> l8_ids = {768716, 760524, 703436, 702924, 695244, 694732, 702668, 694476};
+  for (size_t i = 0; i < 8; i++) {
+    auto l = leading_eight[i];
+    std::cout << "L" << i+1 << " : " << l.Inspect();
+    assert( l.IsDeterministic() == true );
+    assert( l.IsRecipKeep() == true );
+    if (i == 2 || i == 5) {
+      assert( l.IsSecondOrder() == true );
+    } else {
+      assert( l.IsSecondOrder() == false );
+    }
+    assert( l.ID() == l8_ids[i] );
+    assert( Norm::ConstructFromID(l8_ids[i]) == l );
+
+    assert( l.CProb(Reputation::G, Reputation::G) == 1.0 );
+    assert( l.CProb(Reputation::G, Reputation::B) == 0.0 );
+    assert( l.CProb(Reputation::B, Reputation::G) == 1.0 );
+    assert( l.GProbDonor(Reputation::G, Reputation::G, Action::C) == 1.0 );
+    assert( l.GProbDonor(Reputation::G, Reputation::G, Action::D) == 0.0 ); // identification of defectors
+    assert( l.GProbDonor(Reputation::B, Reputation::G, Action::D) == 0.0 ); // identification of defectors
+    assert( l.GProbDonor(Reputation::G, Reputation::B, Action::D) == 1.0 ); // justified punishment
+    assert( l.GProbDonor(Reputation::B, Reputation::G, Action::C) == 1.0 ); // apology
+  }
+
+  // test rescaling
+  {
+    Norm n = Norm::L6();
+    auto rescaled = n.RescaleWithError(0.1, 0.02, 0.0);
+    std::cout << "rescaled: " << rescaled.Inspect() << std::endl;
+    auto expected1 = std::array<double,4>{0.0, 0.9, 0.0, 0.9};
+    bool b1 = IsAllClose(rescaled.P.coop_probs, expected1);
+    assert(b1);
+    auto expected2 = std::array<double,8>{0.98, 0.02, 0.02, 0.98, 0.98, 0.02, 0.02, 0.98};
+    bool b2 = IsAllClose(rescaled.Rd.good_probs, expected2 );
+    assert(b2);
+    auto expected3 = std::array<double,8>{0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0};
+    bool b3 = IsAllClose(rescaled.Rr.good_probs, expected3 );
+    assert(b3);
+  }
+
+  std::cout << "test_Norm passed!" << std::endl;
+}
+
+
+int main() {
+  test_ActionRule();
+  test_AssessmentRule();
+  test_Norm();
+
+  return 0;
+}
