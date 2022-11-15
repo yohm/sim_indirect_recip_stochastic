288ed7f7e60dc8a7e49cd2d56e7ba75c70dc74b2
Yohsuke Murase <yohsuke.murase@gmail.com>
2022-11-15 09:56:17 +0100

    refactoring

--- test_Game.cpp
+++ test_Game.cpp
@@ -18,15 +18,16 @@ bool IsClose(double a, double b, double epsilon = 1.0e-6) {
   return std::abs(a - b) < epsilon;
 }
 
-void test_Game() {
+void test_L8() {
   double mu_e = 0.001, mu_a_donor = 0.001, mu_a_recip = 0.001;
-  std::vector<Norm> norms = {Norm::L1(), Norm::L2(), Norm::L3(), Norm::L4(), Norm::L5(), Norm::L6(), Norm::L7(), Norm::L8()};
-  for (auto n: norms) {
+  std::vector<Norm>
+      norms = {Norm::L1(), Norm::L2(), Norm::L3(), Norm::L4(), Norm::L5(), Norm::L6(), Norm::L7(), Norm::L8()};
+  for (auto n : norms) {
     Game g(mu_e, mu_a_donor, mu_a_recip, n);
     std::cerr << g.r_norm.Inspect();
     IC(g.h_star, g.pc_res_res);
-    assert( g.h_star > 0.97 );
-    assert( g.pc_res_res > 0.96 );
+    assert(g.h_star > 0.97);
+    assert(g.pc_res_res > 0.96);
 
     ActionRule alld = ActionRule::ALLD();
     double H_alld = g.MutantEqReputation(alld);
@@ -36,8 +37,8 @@ void test_Game() {
     double H_allc = g.MutantEqReputation(allc);
     auto pcs_allc = g.MutantCooperationProbs(allc);
     IC(H_allc, pcs_allc);
-    assert( H_alld < 0.01 );
-    assert( H_allc > 0.99 );
+    assert(H_alld < 0.01);
+    assert(H_allc > 0.99);
 
     auto br_alld = g.ESSBenefitRange(alld);
     auto br_allc = g.ESSBenefitRange(allc);
@@ -46,32 +47,35 @@ void test_Game() {
     auto br = g.ESSBenefitRange();
     IC(br);
   }
+  std::cout << "test_L8 passed!" << std::endl;
+}
 
-  {
-    Norm is = Norm::ImageScoring();
-    Game g(mu_e, mu_a_donor, mu_a_recip, is);
-    std::cerr << "IS: " << g.r_norm.Inspect();
-    IC(g.h_star, g.pc_res_res);
-    bool h = IsClose(g.h_star, 0.40, 0.01 );
-    bool pc = IsClose( g.pc_res_res, 0.40, 0.01 );
-    assert( h && pc );
+void test_ImageScoring() {
+  double mu_e = 0.001, mu_a_donor = 0.001, mu_a_recip = 0.001;
+  Norm is = Norm::ImageScoring();
+  Game g(mu_e, mu_a_donor, mu_a_recip, is);
+  std::cerr << "IS: " << g.r_norm.Inspect();
+  IC(g.h_star, g.pc_res_res);
+  bool h = IsClose(g.h_star, 0.40, 0.01 );
+  bool pc = IsClose( g.pc_res_res, 0.40, 0.01 );
+  assert( h && pc );
 
-    ActionRule alld = ActionRule::ALLD();
-    ActionRule allc = ActionRule::ALLC();
-    auto br_alld = g.ESSBenefitRange(alld);
-    auto br_allc = g.ESSBenefitRange(allc);
-    IC(br_alld, br_allc);
+  ActionRule alld = ActionRule::ALLD();
+  ActionRule allc = ActionRule::ALLC();
+  auto br_alld = g.ESSBenefitRange(alld);
+  auto br_allc = g.ESSBenefitRange(allc);
+  IC(br_alld, br_allc);
 
-    auto br = g.ESSBenefitRange();
-    IC(br);
-  }
+  auto br = g.ESSBenefitRange();
+  IC(br);
 
-  std::cout << "test_Game passed!" << std::endl;
+  std::cout << "test_ImageScoring passed!" << std::endl;
 }
 
 
 int main() {
-  test_Game();
+  test_L8();
+  test_ImageScoring();
 
   return 0;
 }
