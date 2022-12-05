a1e4d2ecb14dfda89e26fa3b8a9ca28e7dcd3fff
Yohsuke Murase <yohsuke.murase@gmail.com>
2022-12-05 21:07:20 +0100

    numerical check of analytical forms

--- main_CESS.cpp
+++ main_CESS.cpp
@@ -86,18 +86,17 @@ void EnumerateAllCESS() {
 }
 
 void EnumerateR2() {
-  size_t num = 0, num_passed = 0;
-  const double mu_e = 1.0e-2, mu_a_donor = 1.0e-2, mu_a_recip = 1.0e-2;
 
   std::array<Norm,8> leading_eight = {Norm::L1(), Norm::L2(), Norm::L3(), Norm::L4(),
                                       Norm::L5(), Norm::L6(), Norm::L7(), Norm::L8()};
-
   std::set<int> R2_ids = {
       0b10001000, 0b10001001, 0b10001010, 0b10001011, 0b10001100, 0b10001101, 0b10001110, 0b10001111,
       0b10101000, 0b10101001, 0b10101010, 0b10101011, 0b10101100, 0b10101101, 0b10101110, 0b10101111,
       0b11001000, 0b11001001, 0b11001010, 0b11001011, 0b11001100, 0b11001101, 0b11001110, 0b11001111,
       0b11101000, 0b11101001, 0b11101010, 0b11101011, 0b11101100, 0b11101101, 0b11101110, 0b11101111
   };
+
+  size_t num = 0, num_passed = 0;
   for (Norm norm: leading_eight) {
     // overwrite R2
     std::set<int> cess_ids;
@@ -121,6 +120,98 @@ void EnumerateR2() {
   }
 
   IC(num, num_passed, R2_ids.size());
+}
+
+bool CloseEnough(double a, double b, double tol = 1.0e-2) {
+  return std::abs(a - b) < tol;
+}
+
+void CheckAnalyticFormula() {
+  // using Reputation
+  using Reputation::G, Reputation::B, Action::C, Action::D;
+
+  Norm norm = Norm::L1();
+  norm.Rd.SetGProb(G, B, D, 0.5);
+  norm.Rd.SetGProb(G, B, C, 0.7);
+  norm.Rd.SetGProb(B, G, D, 0.1);
+  norm.Rd.SetGProb(G, G, D, 0.25);
+
+  Game game(1.0e-3, 1.0e-3, 1.0e-3, norm);
+
+  const AssessmentRule R1 = norm.Rd;
+  const AssessmentRule R2 = norm.Rr;
+  const ActionRule P = norm.P;
+  std::cerr << norm.Inspect();
+
+  // R_1(G,G,C) = 1 && R_2(G,G,C) = 1 &&
+  // R_1(G,B,D) + R_2(G,B,D) + R_1(B,G,C) + R_2(B,G,C) > 2  => h* = 1
+  double r = R1.GProb(G,B,D) + R2.GProb(G,B,D)
+      + R1.GProb(B,G,C) + R2.GProb(B,G,C);
+  if (R1.GProb(G,G,C) == 1.0 &&
+      R2.GProb(G,G,C) == 1.0 &&
+      r > 2.0) {
+    if (!CloseEnough(game.h_star, 1.0)) {
+      std::cerr << norm.Inspect();
+      std::cerr << "h* = " << game.h_star << std::endl;
+      assert(false);
+    }
+  }
+  else {
+    std::cerr << "r = " << r << std::endl;
+    std::runtime_error("r is not greater than 2");
+  }
+
+  // P(G,G) = 1
+  if (P.CProb(G,G) == 1.0) {
+    if (!CloseEnough(game.pc_res_res, 1.0)) {
+      std::cerr << "pc_res_res = " << game.pc_res_res << std::endl;
+      assert(false);
+    }
+  }
+
+  // P(G,B) = 0
+  if (norm.CProb(G,B) != 0.0) {
+    std::runtime_error("P(G,B) == 0 is necessary");
+  }
+
+  // P(B,G) = 1
+  if (norm.CProb(B,G) == 1.0) {
+
+    // R_1(B,G,C) > R_1(B,G,D) &&
+    // b/c > (R_1(B,G,D) + R_2(G,B) ) / (R_1(B,G,C) - R_1(B,G,D))
+    if (R1.GProb(B,G,C) > R1.GProb(B,G,D) ) {
+      double b_lower = (R1.GProb(B, G, D) + R2.GProb(G, B, D)) / (R1.GProb(B, G, C) - R1.GProb(B, G, D));
+      IC(b_lower);
+    }
+    else {
+      std::runtime_error("R_1(B,G,C) > R_1(B,G,D) is necessary");
+    }
+
+    // R_1(G,G,D) < 1   &&
+    // b/c > ( R_1(B,G,C) + R_2(G,B) ) / ( R_1(G,G,C) - R_1(G,G,D) )
+    if (R1.GProb(G,G,D) < 1.0) {
+      double b_lower = (R1.GProb(B, G, C) + R2.GProb(G, B, D)) / (R1.GProb(G, G, C) - R1.GProb(G, G, D));
+      IC(b_lower);
+    }
+    else {
+      std::runtime_error("R_1(G,G,D) < 1 is necessary");
+    }
+
+    // R_1(G,B,C) <= R_1(G,B,D) ||
+    // b/c < (R_1(B,G,C) + R_2(G,B) ) / ( R_1(G,B,C) - R_1(G,B,D) )
+    if (R1.GProb(G,B,C) <= R1.GProb(G,B,D)) {
+      double b_upper = std::numeric_limits<double>::infinity();
+      IC(b_upper);
+    }
+    else {
+      double b_upper = (R1.GProb(B, G, C) + R2.GProb(G, B, D)) / (R1.GProb(G, B, C) - R1.GProb(G, B, D));
+      IC(b_upper);
+    }
+  }
+
+
+  auto [isCESS, brange] = CheckCESS(norm);
+  IC(isCESS, brange);
 }
 
 void FindMutant() {
@@ -149,8 +240,10 @@ void FindMutant() {
 
 int main() {
   // FindLeadingEight();
-  EnumerateAllCESS();
+  // EnumerateAllCESS();
   // EnumerateR2();
+
+  CheckAnalyticFormula();
 
   // FindMutant();
 
