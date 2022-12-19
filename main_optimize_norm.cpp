9b81e31aef0b260ed2e2f53767c89c0f32f91f6d
Yohsuke Murase <yohsuke.murase@gmail.com>
2022-12-19 15:33:28 +0100

    initial import of pagmo optimization program

---
+++ main_optimize_norm.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <pagmo/algorithms/pso_gen.hpp>
+#include <pagmo/problem.hpp>
+#include <pagmo/batch_evaluators/member_bfe.hpp>
+#include <mpi.h>
+#include <pagmo/problems/schwefel.hpp>
+#include "Norm.hpp"
+#include "PrivRepGame.hpp"
+
+
+using namespace pagmo;
+
+int main(int argc, char* argv[]) {
+
+  MPI_Init(&argc, &argv);
+  int my_rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+
+  struct my_problem {
+
+    // Implementation of the objective function.
+    vector_double fitness(const vector_double &dv) const
+    {
+      // [TODO] implement me
+      return {0.0};
+    }
+
+    vector_double batch_fitness(const vector_double& dvs) const {
+      size_t input_dim = get_bounds().first.size();
+      // assert( output_dim == 1 );
+      size_t num_dvs = dvs.size() / input_dim;
+      vector_double local_fitness(num_dvs, 0.0);
+
+      int rank, num_procs;
+      MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
+      MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+      // std::cerr << "batch_fitness called: " << rank << '/' << num_procs << std::endl;
+
+      for (size_t i = rank; i < num_dvs; i+=num_procs) {
+        size_t idx = i * input_dim;
+        auto first = dvs.begin() + idx;
+        auto last = dvs.begin() + idx + input_dim;
+        std::vector<double> dv(first, last);
+        double f = fitness(dv)[0];
+        local_fitness[i] = f;
+      }
+
+      vector_double fitness(local_fitness.size(), 0.0);
+      MPI_Allreduce(local_fitness.data(), fitness.data(), local_fitness.size(), MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+
+      // std::cerr << "batch_fitness done: " << rank << '/' << num_procs << std::endl;
+
+      return fitness;
+    }
+
+    // Implementation of the box bounds.
+    std::pair<vector_double, vector_double> get_bounds() const
+    {
+      constexpr size_t input_dim = 12;
+      return {std::vector<double>(input_dim, 0.0), std::vector<double>(input_dim, 1.0)};
+    }
+  };
+
+  problem prob{my_problem{}};
+
+  // 2 - Instantiate a pagmo algorithm
+  pagmo::random_device::set_seed(12345);
+  unsigned max_gen = 100u;
+  pso_gen pso_algo{max_gen};
+  pso_algo.set_bfe(bfe{member_bfe{}});
+  algorithm algo{pso_algo};
+  if (my_rank == 0) algo.set_verbosity(max_gen / 10);
+
+  // 3 - Instantiate a population of size 50
+  population pop{prob, member_bfe{}, 50, 123456};
+
+  // 4 - Let population evolve
+  auto start = std::chrono::high_resolution_clock::now();
+  pop = algo.evolve(pop);
+  if( my_rank == 0) {
+    std::cout << pop.champion_f()[0] << "\n";
+    for (auto x : pop.champion_x()) {
+      std::cout << x << ' ';
+    }
+  }
+  auto end = std::chrono::high_resolution_clock::now();
+
+  if (my_rank == 0) {
+    std::cout << "Elapsed time: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms\n";
+  }
+
+  MPI_Finalize();
+
+  return 0;
+}
+
