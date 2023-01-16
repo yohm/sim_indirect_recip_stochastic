2ec8017bb75071d8973cca6f820cd24af1e32b26
Yohsuke Murase <yohsuke.murase@gmail.com>
2023-01-16 15:06:48 +0100

    implemented comprehensive search on deterministic norms

--- main_priv_sim.cpp
+++ main_priv_sim.cpp
@@ -11,63 +11,83 @@ constexpr Reputation B = Reputation::B, G = Reputation::G;
 constexpr Action C = Action::C, D = Action::D;
 
 
-int main() {
-
-  // ActionRule ar({
-  //                   {{G,G}, C},
-  //                   {{G,B}, D},
-  //                   {{B,G}, C},
-  //                   {{B,B}, D},
-  //               });
-
-  // AssessmentRule Rd({
-  //                       {{G,G,C}, 1.0}, {{G,G,D}, 0.0},
-  //                       //{{G,B,C}, 0.5}, {{G,B,D}, 0.5},
-  //                       {{G,B,C}, 1.0}, {{G,B,D}, 1.0},
-  //                       {{B,G,C}, 1.0}, {{B,G,D}, 0.0},
-  //                       {{B,B,C}, 1.0}, {{B,B,D}, 1.0},
-  //                   });
-  // AssessmentRule Rr = AssessmentRule::ImageScoring();
-  // AssessmentRule Rr = AssessmentRule::KeepRecipient();
-
-  // Norm norm(Rd, Rr, ar);
-  // Norm norm = Norm::L1();
-  // norm.Rr = AssessmentRule::ImageScoring();
-
-  // PrivateRepGame priv_game( {{norm, 90}}, 123456789ull);
-  // priv_game.Update(1000000, 0.9, 0.05);
-  // // IC(priv_game.CoopCount(), priv_game.GameCount());
-  // IC(priv_game.SystemWideCooperationLevel());
-
-  /*
-  IC(Norm::L1().Inspect());
-  PrivateRepGame priv_game( {{Norm::L1(), 30}, {Norm::AllC(), 30}, {Norm::AllD(), 30}}, 123456789ull);
-  priv_game.Update(1e6, 0.9, 0.05);
-  priv_game.ResetCounts();
-  priv_game.Update(1e6, 0.9, 0.05);
-  IC( priv_game.NormCooperationLevels(), priv_game.NormAverageReputation() );
-  // priv_game.PrintImage(std::cerr);
-   */
-
-  Norm norm = Norm::L3();
-  // norm.Rd.SetGProb(G, B, D, 0.5);
-  // norm.Rd.SetGProb(G, B, C, 0.5);
-  // norm.Rr = AssessmentRule::ImageScoring();
-  // norm.Rr = AssessmentRule::KeepRecipient();
-
-  PrivateRepGame priv_game( {{norm, 50}}, 123456789ull);
-  priv_game.Update(1e6, 0.9, 0.05, false);
-  priv_game.ResetCounts();
-  priv_game.Update(1e6, 0.9, 0.05, false);
-  IC( priv_game.NormCooperationLevels() );
-
+void ComprehensiveSearchWithoutR2() {
   EvolPrivRepGame::SimulationParameters params;
+  params.n_init = 1e5;
+  params.n_steps = 1e5;
+  EvolPrivRepGameAllCAllD evol(30, params, 5.0, 1.0);
 
-  EvolPrivRepGame evol(50, {norm, Norm::AllC(), Norm::AllD()}, params);
-  auto rhos = evol.FixationProbabilities(5.0, 1.0);
-  IC(rhos);
-  auto eq = evol.EquilibriumPopulationLowMut(rhos);
-  IC(eq);
+  std::vector< std::pair<int,double> > results;
+
+  for (int j = 0; j < 256; j++) {
+    std::cerr << "j = " << j << std::endl;
+    AssessmentRule R1 = AssessmentRule::MakeDeterministicRule(j);
+    AssessmentRule R2 = AssessmentRule::KeepRecipient();
+
+    for (int i = 0; i < 16; i++) {
+      ActionRule P = ActionRule::MakeDeterministicRule(i);
+      Norm norm(R1, R2, P);
+      if (norm.ID() < norm.SwapGB().ID()) {
+        continue;
+      }
+
+      auto pc_rho_eq = evol.EquilibriumCoopLevelAllCAllD(norm);
+      double self_cooperation_level = std::get<0>(pc_rho_eq);
+      auto eq = std::get<2>(pc_rho_eq);
+      double c = self_cooperation_level * eq[0] + 1.0 * eq[1];
+      results.emplace_back(norm.ID(), c);
+    }
+  }
+
+  // sort results by cooperation level
+  std::sort(results.begin(), results.end(), [](const auto& a, const auto& b) { return a.second > b.second; });
+  // print top 10 from results
+  for (int i = 0; i < 100; i++) {
+    Norm norm = Norm::ConstructFromID(results[i].first);
+    std::cout << norm.Inspect() << " " << results[i].second << std::endl;
+  }
+}
+
+void ComprehensiveSearchWithR2() {
+  EvolPrivRepGame::SimulationParameters params;
+  params.n_init = 1e5;
+  params.n_steps = 1e5;
+  EvolPrivRepGameAllCAllD evol(30, params, 5.0, 1.0);
+
+  std::vector< std::pair<int,double> > results;
+
+  for (int j = 0; j < 65536; j++) {
+    std::cerr << "j = " << j << std::endl;
+    AssessmentRule R1 = AssessmentRule::MakeDeterministicRule(j / 256);
+    AssessmentRule R2 = AssessmentRule::MakeDeterministicRule(j % 256);
+
+    for (int i = 0; i < 16; i++) {
+      ActionRule P = ActionRule::MakeDeterministicRule(i);
+      Norm norm(R1, R2, P);
+      if (norm.ID() < norm.SwapGB().ID()) {
+        continue;
+      }
+
+      auto pc_rho_eq = evol.EquilibriumCoopLevelAllCAllD(norm);
+      double self_cooperation_level = std::get<0>(pc_rho_eq);
+      auto eq = std::get<2>(pc_rho_eq);
+      double c = self_cooperation_level * eq[0] + 1.0 * eq[1];
+      results.emplace_back(norm.ID(), c);
+    }
+  }
+
+  // sort results by cooperation level
+  std::sort(results.begin(), results.end(), [](const auto& a, const auto& b) { return a.second > b.second; });
+  // print top 10 from results
+  for (int i = 0; i < 100; i++) {
+    Norm norm = Norm::ConstructFromID(results[i].first);
+    std::cout << norm.Inspect() << " " << results[i].second << std::endl;
+  }
+}
+
+
+int main() {
+  ComprehensiveSearchWithoutR2();
 
   return 0;
 }
